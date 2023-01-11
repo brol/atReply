95a3173f75a6b4e25571ea20074f603598433346
Pierre <brol31@gmail.com>
2023-01-11 13:02:47 +0100

    2.0-dev suite

--- locales/fr/admin.lang.php
+++ locales/fr/admin.lang.php
@@ -1,63 +1,31 @@
 <?php
-// Language: Français 
-// Module: atReply - 1.8.3
-// Date: 2018-04-05 00:58:13 
-// Translated with dcTranslater - 2018.02.14 
+// Language: Français
+// Module: atReply - 
+// Date: 2023-01-11 12:01:00
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
 
-#_admin.php:75
-$GLOBALS['__l10n']['@ Reply:'] = '@ Reply&nbsp;:';
 
-#_admin.php:77
-$GLOBALS['__l10n']['Please edit and publish it.'] = 'Veuillez l\'éditer et le publier.';
+#
+# DOT NOT MODIFY THIS FILE !
+#
 
-#_admin.php:89
-$GLOBALS['__l10n']['Create a new comment in reply to this comment'] = 'Créer un nouveau commentaire en réponse à ce commentaire';
-
-#index.php:131
-$GLOBALS['__l10n']['%s is not writable'] = '%s n\'est pas accessible en écriture';
-
-#index.php:231
-$GLOBALS['__l10n']['Add arrows to easily reply to comments on the blog'] = 'Ajouter des flèches pour répondre facilement aux commentaires sur le blog';
-
-#index.php:237
-$GLOBALS['__l10n']['Activating this plugin also enforces wiki syntax in blog comments.'] = 'L\'activation de ce plugin force la syntaxe wiki dans les commentaires du blog.';
-
-#index.php:245
-$GLOBALS['__l10n']['Display a text when the cursor is hovering the arrow'] = 'Afficher un texte quand le curseur survole la flèche';
-
-#index.php:252
-#index.php:264
-$GLOBALS['__l10n']['Append replies to appropriate comments'] = 'Attacher les réponses aux commentaires afférents';
-
-#index.php:259
-$GLOBALS['__l10n']['Display a switch to toggle threading'] = 'Afficher un bouton pour basculer l\'affichage en fil de commentaires';
-
-#index.php:263
-$GLOBALS['__l10n']['Requires "%s".'] = 'Nécessite "%s".';
-
-#index.php:270
-$GLOBALS['__l10n']['When clicking on the "%s" button in a comment list of the administration, subscribe to comments the email address of the replied comment with the %s plugin'] = 'Lors du clic sur le bouton "%s" dans une liste de commentaires de l\'administration, abonner aux commentaires l\'adresse email du commentaire répondu avec le plugin %s';
-
-#index.php:271
-$GLOBALS['__l10n']['Reply to this comment'] = 'Répondre à ce commentaire';
-
-#index.php:275
-$GLOBALS['__l10n']['Requires the %s plugin.'] = 'Le plugin %s est requis.';
-
-#index.php:281
-$GLOBALS['__l10n']['Create an image with another color'] = 'Créer une image avec une autre couleur';
-
-#index.php:287
-$GLOBALS['__l10n']['Leave blank to disable this feature.'] = 'Laissez vide pour annuler ce comportement.';
-
-#index.php:288
-$GLOBALS['__l10n']['The default image will be used.'] = 'L\'image par défaut sera utilisée.';
-
-#index.php:291
-$GLOBALS['__l10n']['Preview:'] = 'Aperçu&nbsp;:';
-
-#index.php:294
-$GLOBALS['__l10n']['Visitors may see the old image if their browser still use it.'] = 'Les visiteurs peuvent voir l\'ancienne image si leur navigateur l\'utilise toujours.';
-
-$GLOBALS['__l10n']['Easily reply to comments'] = 'Répondre facilement aux commentaires';
-
+l10n::$locales['@ Reply:'] = '@ Reply&nbsp;:';
+l10n::$locales['Please edit and publish it.'] = 'Veuillez l\'éditer et le publier.';
+l10n::$locales['Create a new comment in reply to this comment'] = 'Créer un nouveau commentaire en réponse à ce commentaire';
+l10n::$locales['%s is not writable'] = '%s n\'est pas accessible en écriture';
+l10n::$locales['Add arrows to easily reply to comments on the blog'] = 'Ajouter des flèches pour répondre facilement aux commentaires sur le blog';
+l10n::$locales['Activating this plugin also enforces wiki syntax in blog comments.'] = 'L\'activation de ce plugin force la syntaxe wiki dans les commentaires du blog.';
+l10n::$locales['Display a text when the cursor is hovering the arrow'] = 'Afficher un texte quand le curseur survole la flèche';
+l10n::$locales['Append replies to appropriate comments'] = 'Attacher les réponses aux commentaires afférents';
+l10n::$locales['Display a switch to toggle threading'] = 'Afficher un bouton pour basculer l\'affichage en fil de commentaires';
+l10n::$locales['Requires "%s".'] = 'Nécessite "%s".';
+l10n::$locales['When clicking on the "%s" button in a comment list of the administration, subscribe to comments the email address of the replied comment with the %s plugin'] = 'Lors du clic sur le bouton "%s" dans une liste de commentaires de l\'administration, abonner aux commentaires l\'adresse email du commentaire répondu avec le plugin %s';
+l10n::$locales['Reply to this comment'] = 'Répondre à ce commentaire';
+l10n::$locales['Requires the %s plugin.'] = 'Le plugin %s est requis.';
+l10n::$locales['Create an image with another color'] = 'Créer une image avec une autre couleur';
+l10n::$locales['Leave blank to disable this feature.'] = 'Laissez vide pour annuler ce comportement.';
+l10n::$locales['The default image will be used.'] = 'L\'image par défaut sera utilisée.';
+l10n::$locales['Preview:'] = 'Aperçu&nbsp;:';
+l10n::$locales['Visitors may see the old image if their browser still use it.'] = 'Les visiteurs peuvent voir l\'ancienne image si leur navigateur l\'utilise toujours.';
+l10n::$locales['Easily reply to comments'] = 'Répondre facilement aux commentaires';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -1,23 +1,17 @@
 <?php
-// Language: Français 
-// Module: atReply - 1.8.3
-// Date: 2018-04-05 00:58:13 
-// Translated with dcTranslater - 2018.02.14 
+// Language: Français
+// Module: atReply - 
+// Date: 2023-01-11 12:01:00
+// Author: Pierre Van Glabeke
+// Translated with dcTranslater - 2022.12.26
 
-#_admin.php:53
-#_admin.php:169
-#index.php:42
-$GLOBALS['__l10n']['@ Reply'] = '@ Reply';
 
-#_admin.php:123
-$GLOBALS['__l10n']['@%s:'] = '@%s&nbsp;:';
+#
+# DOT NOT MODIFY THIS FILE !
+#
 
-#_public.php:92
-$GLOBALS['__l10n']['Reply to comment {author}'] = 'Répondre au commentaire de {author}';
-
-#_public.php:95
-$GLOBALS['__l10n']['Threaded comments'] = 'Fil de discussion';
-
-#index.php:241
-$GLOBALS['__l10n']['Settings'] = 'Paramètres';
-
+l10n::$locales['@ Reply'] = '@ Reply';
+l10n::$locales['@%s:'] = '@%s&nbsp;:';
+l10n::$locales['Reply to comment {author}'] = 'Répondre au commentaire de {author}';
+l10n::$locales['Threaded comments'] = 'Fil de discussion';
+l10n::$locales['Settings'] = 'Paramètres';
